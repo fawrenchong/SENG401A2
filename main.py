21f4f629096b2c01a9b93307cd2f602f0ba159bf
Fa Wren Chong <fwc14@uclive.ac.nz>
2024-04-22 19:27:40 +1200

    Retrieved project from gitlab

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import dotenv
+import gitlab
+
+dotenv.load_dotenv(override=True)
+TOKEN = os.environ['TOKEN']
+gl = gitlab.Gitlab('https://eng-git.canterbury.ac.nz/', private_token=TOKEN)
+
+if __name__ == '__main__':
+    project_id = 15303
+    project = gl.projects.get(project_id)
+    print(project)
